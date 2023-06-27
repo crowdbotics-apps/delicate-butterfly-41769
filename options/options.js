45394a5d9e3e6d14ab0696f6105fbcba19eb5916
Crowdbotics <team@crowdbotics.com>
2023-06-27 20:12:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-butterfly-41769",
+  url: "https://delicate-butterfly-41769.botics.co",
+  api: "https://delicate-butterfly-41769.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
